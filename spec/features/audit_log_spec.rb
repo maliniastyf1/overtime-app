5b6219f70b252d28c89c99d5410e66822d5cac3e
maliniastyf1 <malinowski.adam85@gmail.com>
2017-02-12 13:59:07 +0100

    Blocked non admin users from accessing index page for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -18,8 +18,14 @@ describe 'Audit Logs Feature' do
       expect(page).to have_content(/MALYN, ADAM/)
     end
 
-    xit 'can not be accessed by non admin users' do
+    it 'can not be accessed by non admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
 
+      visit audit_logs_path
+
+      expect(current_path).to eq(root_path)
     end
 
   end
