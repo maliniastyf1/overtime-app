4422591d71191db0e0d7f399275e47edcb8f7014
maliniastyf1 <malinowski.adam85@gmail.com>
2017-02-22 21:58:05 +0100

    Built out rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user = User.create(email: "adam@test.com",
+@employee = Employee.create(email: "adam@test.com",
                     password: "qwerty",
                     password_confirmation: "qwerty",
                     first_name: "Adam",
@@ -20,16 +20,16 @@ puts "1 admin user created"
   Post.create!(date: Date.today,
                rationale: "#{post} rationel contentNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
                 Nie ma nikogo, kto lubiłby ból dla samego bólu, szukał go tylko po to, by go poczuć, po prostu dlatego, że to ból",
-               user_id: @user.id,
+               user_id: @employee.id,
                overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
 
 
-  AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-  AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-  AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+  AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+  AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 
 puts "3 Audit logs have been created"
